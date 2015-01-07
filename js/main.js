f42313f2807638086e25c46f23417934efa752d5
miloroot <milonice@hotmail.com>
2015-01-07 17:28:27 +0100

    The forms values are now getting fetched. Being sent to console and the DOM.

--- js/main.js
+++ js/main.js
@@ -20,8 +20,11 @@ $(function() {
 	$('.createChar').submit(function() {
 		var charName = $('.charName').val();
 		var charClass = $('.chooseClass').val();
-		
+
 		console.log(charName + ' - ' + charClass);
+
+		$('.confirmation').append('You chose the name: ' + charName + '.' + ' With the class: ' + charClass + '.<br>');
+		$('.confirmation').append('If this is correct. Click the button. <br> <button class="btn2">Confirm!</button>');
 
 		return false;
 	});
